76e9dd3e6252b16458205fe1daca0ac4fcbdb081
Hernan David <hernan.plazabs@gmail.com>
2022-04-04 16:45:39 -0500

    first commit

---
+++ README2.md
@@ -0,0 +1,7 @@
+You need to run this app:
+
+# node v14.17.0 version
+
+# electron v11.5.0 version
+
+# iohook: 0.9.0 version
---
+++ demo-ani.js
@@ -0,0 +1,5 @@
+const nodeAbi = require("node-abi");
+
+console.log(nodeAbi.getAbi("14.17.0", "node"));
+
+console.log(nodeAbi.getAbi("11.5.0", "electron"));
